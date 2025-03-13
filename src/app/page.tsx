da92e3c31d378ea65a7b3d90a2ac21cfa9dfe92a
dd <the.jesse.dev@gmail.com>
2025-03-13 18:36:15 +0800

    moved stuff around

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Suspense } from "react";
 import { api, HydrateClient } from "~/trpc/server";
-import { VTable } from "./_components/VTable";
+import { VTable } from "~/components/VTable";
 
 export default async function Home() {
   void api.vtable.getTable.prefetch({ id: 1 });
