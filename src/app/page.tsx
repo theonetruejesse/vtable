6c20a579422360cece8d58b9e522a42157271fa2
dd <the.jesse.dev@gmail.com>
2025-03-13 14:15:55 +0800

    basic table together

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@ import { api, HydrateClient } from "~/trpc/server";
 import { VTable } from "./_components/vtable";
 
 export default async function Home() {
-  void api.vtable.getTable.prefetch({ id: 1 });
+  const data = await api.vtable.getTable({ id: 1 }); // TODO: change pattern
   return (
     <HydrateClient>
       <main>
         <div className="flex flex-col items-center justify-center">
           <h1 className="text-4xl font-bold">hello world</h1>
           <Suspense fallback={<div>Loading...</div>}>
-            <VTable />
+            {data && <VTable data={data} />}
           </Suspense>
         </div>
       </main>
